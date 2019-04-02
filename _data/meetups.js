3033b83017a1e961f77809c64d864e98312cc695
Bryan Robinson <bryanlrobinson@gmail.com>
2019-04-01 21:29:22 -0500

    Fixes meetup.js to not write to file and to return data

--- _data/meetups.js
+++ _data/meetups.js
@@ -1,11 +1,10 @@
 var project = require('../gulp-src/_project.js');
 var axios   = require('axios');
 
-module.exports = function() {
+module.exports = async function() {
     let url = `https://api.meetup.com/${process.env.MEETUP_URL }/events?photo-host=public&page=20&sig_id=${process.env.MEETUP_KEY}`;
-    let statusFile = "_data/meetups.json";
     let stringMatch = ""; // Match meetup name against a string if you need to filter
-    axios.get(url)
+    return axios.get(url)
         .then(function (response) {
             let data = response.data;
             if (stringMatch.length > 0) {
@@ -14,7 +13,7 @@ module.exports = function() {
                     return meetup.name.includes(stringMatch);
                 });
             }
-            return project.storeData(statusFile, JSON.stringify(data));
+            return data;
         })
         .catch(function(error) {
             console.log(error);
